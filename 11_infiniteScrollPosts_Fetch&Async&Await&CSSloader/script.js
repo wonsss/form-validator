cc11df8878e82e02892b4dfeb07fdea06bfd9fc4
wonsss <bigdatajang@gmail.com>
2021-10-23 00:08:43 +0900

    #11  Filter posts by input

--- 11_infiniteScrollPosts_Fetch&Async&Await&CSSloader/script.js
+++ 11_infiniteScrollPosts_Fetch&Async&Await&CSSloader/script.js
@@ -47,6 +47,21 @@ function showLoading() {
   }, 300); //로더가 보이는 시간
 }
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+  posts.forEach((post) => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
 // Show initial posts
 showPosts();
 
@@ -57,3 +72,5 @@ window.addEventListener('scroll', () => {
     showLoading();
   }
 });
+
+filter.addEventListener('input', filterPosts);
