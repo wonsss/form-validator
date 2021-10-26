58fac6f442992e3eb5ef597b68c02f409a88a6dd
wonsss <bigdatajang@gmail.com>
2021-10-26 11:33:11 +0900

    #12 Difficulty Setting

--- 12_typingGame_DOM&Intervals&Events/script.js
+++ 12_typingGame_DOM&Intervals&Events/script.js
@@ -41,6 +41,18 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Set difficulty to value in ls or medium
+let difficulty =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// Set difficulty select value
+difficultySelect.value =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
 // Focus on text on start
 text.focus();
 
@@ -88,6 +100,8 @@ function gameOver() {
 addWordToDOM();
 
 // Event Listeners
+
+// Typing
 text.addEventListener('input', (e) => {
   const insertedText = e.target.value;
   if (insertedText === randomWord) {
@@ -95,7 +109,23 @@ text.addEventListener('input', (e) => {
     updateScore();
     // Clear
     e.target.value = '';
-    time += 2;
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
+    }
     updateTime();
   }
+});
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
+  console.log(difficulty);
 });
