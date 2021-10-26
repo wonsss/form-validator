12e5a22d644afeb6852df36d85013e6f35ede56a
wonsss <bigdatajang@gmail.com>
2021-10-26 16:40:16 +0900

    #14 Set&Get card data from local storage

--- 14_MemoryCards_CSSEffects&LocalStorage/script.js
+++ 14_MemoryCards_CSSEffects&LocalStorage/script.js
@@ -17,20 +17,22 @@ let currentActiveCard = 0;
 const cardsEl = [];
 
 // Store card data
-const cardsData = [
-  {
-    question: 'question1',
-    answer: 'answer1',
-  },
-  {
-    question: 'question2',
-    answer: 'answer2',
-  },
-  {
-    question: 'question3',
-    answer: 'answer3',
-  },
-];
+const cardsData = getCardsData();
+
+// const cardsData = [
+//   {
+//     question: 'question1',
+//     answer: 'answer1',
+//   },
+//   {
+//     question: 'question2',
+//     answer: 'answer2',
+//   },
+//   {
+//     question: 'question3',
+//     answer: 'answer3',
+//   },
+// ];
 
 // Create all cards
 function createCards() {
@@ -69,9 +71,21 @@ function updateCurrentText() {
   currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
+// Get cards from local storage
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem('cards'));
+  return cards === null ? [] : cards;
+}
+
+// Add card to local storage
+function setCardsData(cards) {
+  localStorage.setItem('cards', JSON.stringify(cards));
+  window.location.reload();
+}
+
 createCards();
 
-// Event listeners
+// Next Btn
 nextBtn.addEventListener('click', () => {
   cardsEl[currentActiveCard].className = 'card left';
   currentActiveCard = currentActiveCard + 1;
@@ -82,6 +96,7 @@ nextBtn.addEventListener('click', () => {
   updateCurrentText();
 });
 
+// Prev Btn
 prevBtn.addEventListener('click', () => {
   cardsEl[currentActiveCard].className = 'card right';
   currentActiveCard = currentActiveCard - 1;
@@ -90,4 +105,32 @@ prevBtn.addEventListener('click', () => {
   }
   cardsEl[currentActiveCard].className = 'card active';
   updateCurrentText();
+});
+
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+// Hide add container
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+
+// Add new card
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer };
+    createCard(newCard);
+    questionEl.value = '';
+    answerEl.value = '';
+    addContainer.classList.remove('show');
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
+});
+
+// Clear cards button
+clearBtn.addEventListener('click', () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = '';
+  window.location.reload();
 });
