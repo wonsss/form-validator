2482a6924e0627b1fa94f93093979f987b8f19eb
wonsss <bigdatajang@gmail.com>
2021-10-26 22:51:55 +0900

    #15 Get & Display song lyrics

--- 15_LyricsSearchApp_Fetch&Pagination&LyricsOvhAPI/script.js
+++ 15_LyricsSearchApp_Fetch&Pagination&LyricsOvhAPI/script.js
@@ -48,10 +48,20 @@ function showData(data) {
 }
 
 async function getMoreSongs(url) {
-  setTimeout(() => {}, 1000);
   const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
   const data = await res.json();
   showData(data);
+}
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}
+  </h2><span>${lyrics}</span>`;
+  more.innerHTML = '';
 }
 
 // Event listeners
@@ -62,5 +72,16 @@ form.addEventListener('submit', (e) => {
     alert('Please type in a search term');
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+// Get lyrics button click
+result.addEventListener('click', (e) => {
+  const clickedEl = e.target;
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
   }
 });
