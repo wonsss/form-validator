02bf144ff8f6e02d5a0bb56981d8a71bb60ac7ef
wonsss <bigdatajang@gmail.com>
2021-10-27 11:09:53 +0900

    #16 Breath Animation with JS trigger

---
+++ 16_relaxerApp_CSSAnimation&setTimeout/script.js
@@ -0,0 +1,23 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+console.log(breatheTime, holdTime);
+
+function breatheAnimation() {
+  text.innerText = 'Breathe In';
+  container.className = 'container grow';
+  setTimeout(() => {
+    text.innerText = 'Hold';
+    setTimeout(() => {
+      text.innerText = 'Breathe Out';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+breatheAnimation();
+
+setInterval(breatheAnimation, totalTime);
