2017ddd999cefc3fba9df29cf7f2e2238ccd287d
wonsss <bigdatajang@gmail.com>
2021-10-28 15:46:55 +0900

    #20-3 Check Guessed Number

--- 20_speakNumberGuessingGame_speechRecognition/script.js
+++ 20_speakNumberGuessingGame_speechRecognition/script.js
@@ -12,6 +12,46 @@ recognition.start();
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
   console.log(msg);
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+        <div>You said: </div>
+        <span class="box">${msg}</span>
+    `;
+}
+
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if valid number
+  if (isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
+    return;
+  }
+
+  // Check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += `<div>Number must be between 1 and 100</div>`;
+    return;
+  }
+
+  // Check Number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+        <h2>Congrats! <br> You have guessed the number! <br><br>
+        It was ${num}</h2>
+        <button class="play-again" id="play-again">Play Again</button>
+        `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>GO LOWER</div>';
+  } else if (num < randomNum) {
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
+  }
 }
 
 // Generate random number
@@ -21,3 +61,13 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+// Play again
+document.body.addEventListener('click', (e) => {
+  if (e.target.id == 'play-again') {
+    window.location.reload();
+  }
+});
